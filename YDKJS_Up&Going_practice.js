f5d98b98fecf30440928126ce28b98fb8aed7f05
BuildTools <Skarrix.R@gmail.com>
2017-04-27 22:21:21 +0200

    Fix syntax

--- YDKJS_Up&Going_practice.js
+++ YDKJS_Up&Going_practice.js
@@ -3,9 +3,9 @@ const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
 var bank_balance = prompt("Please enter bank balance");
 const SPENDING = prompt("What's your spending threshold?");
-var amount = PHONE_PRICE;
-numOfPhones = 0;
-numOfAccessories = 0;
+var amount = 0;
+var numOfPhones = 0;
+var numOfAccessories = 0;
 
 function buyPhone() {
   console.log("I want to buy this phone.");
@@ -21,7 +21,7 @@ function buyAccessory() {
   console.log(numOfAccessories);
 }
 
-function addTax() {
+function addTax(amount) {
   amount = amount + (amount * TAX_RATE); //adds tax
 }
 
